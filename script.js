bb12b4e10ac09384177ccadb29fad7102ba4d424
isnendyan <isnendyankp@gmail.com>
2023-06-04 09:40:07 +0700

    S18-174: Check children to rebuildArrays

--- script.js
+++ script.js
@@ -96,6 +96,12 @@ function updateDOM() {
 
 }
 
+// Allows arrays to reflect Drag and Drop items
+function rebuildArrays() {
+  console.log(backlogList.children);
+  console.log(progressList.children);
+}
+
 // When Item Starts Dragging
 function drag(e) {
   draggedItem = e.target;
@@ -124,6 +130,7 @@ function drop(e) {
   // Add item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 }
 
 
