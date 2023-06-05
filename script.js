73afd21f73f94aaa572d968061ccd13eafaac8d1
isnendyan <isnendyankp@gmail.com>
2023-06-05 07:16:11 +0700

    S18-175: Create function Show&Hide Item

--- script.js
+++ script.js
@@ -96,6 +96,21 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// Show Add Item Input Box
+function showInputBox(column) {
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+// Hide Item Input Box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  
+}
+
 // Allows arrays to reflect Drag and Drop items
 function rebuildArrays() {
   backlogListArray = [];
