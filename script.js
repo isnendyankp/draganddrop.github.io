ddd8e642c504e8c0220a16602beead1b319aa2db
isnendyan <isnendyankp@gmail.com>
2023-06-07 08:09:15 +0700

    S18-176: Create Funct Delete item on text content

--- script.js
+++ script.js
@@ -98,10 +98,14 @@ function updateDOM() {
 // Update Item - Delete if necessary, or update Array value
 function updateItem(id, column) {
   const selectedArray = listArrays[column];
-  console.log(selectedArray);
   const selectedColumnEl = listColumns[column].children;
-  console.log(selectedColumnEl[id].textContent);
+    if (!selectedColumnEl[id].textContent) {
+      delete selectedArray[id];
+    }
+    console.log(selectedArray); 
+    updateDOM();
 }
+
 
 // Add to Column List, Reset Textbox
 function addToColumn(column) {
